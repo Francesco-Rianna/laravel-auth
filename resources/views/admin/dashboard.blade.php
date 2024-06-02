655063bee32672569833398d2b20f358e9581722
Francesco-Rianna <maradonino98@hotmail.it>
2024-06-02 17:48:17 +0200

    inserted data in dashboard

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -8,7 +8,8 @@
     <div class="row justify-content-center">
         <div class="col">
             <div class="card">
-                <div class="card-header">{{ __('User Dashboard') }}</div>
+                <div class="card-header"> {{ __('User Dashboard') }}</div>
+ 
 
                 <div class="card-body">
                     @if (session('status'))
@@ -18,6 +19,7 @@
                     @endif
 
                     {{ __('You are logged in!') }}
+                   <div> Benvenuto  {{$user->name}}</div>
                 </div>
             </div>
         </div>
